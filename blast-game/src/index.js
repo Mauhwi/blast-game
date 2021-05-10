040aadbbcea0edd5a968948bc2c9ae9d10798ce7
Mauhwi <tykityki7@gmail.com>
2021-05-11 04:49:11 +0500

    без рекурсии в ItemFall, added Images Assets

--- blast-game/src/index.js
+++ blast-game/src/index.js
@@ -1,34 +1,51 @@
+import Blue from './blue.png';
+import Green from './green.png';
+import Purple from './purple.png';
+import Red from './red.png';
+import Yellow from './yellow.png';
+
+
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
-let playFieldWidth = 600;
-let playFieldHeight = 600;
-let cols =4;
-let rows =4;
-let k=2
-let c=3;
+let playFieldWidth = 400;
+let playFieldHeight = 400;
+canvas.width = playFieldWidth;
+canvas.height = playFieldHeight;
 
-let itemSize = 100;
+let cols = 5;
+let rows = 5;
+let k = 2;
+let c = 4;
 
-let mouse = {
-  x: undefined,
-  y: undefined,
-};
+let itemSize = playFieldHeight / rows;
+
+//условия
+let movesToWin = 10;
+let itemsToDestroy = 30;
 
 //цвета
-let green = "rgb(0,153,76)";
-let red = "rgb(153,0,0)";
-let blue = "rgb(0,102,204)";
-let pink = "rgb(153,0,76)";
-let grey = "rgb(60,70,76)";
-let fillColors = [green, red, blue, pink, grey];
+let blue = new Image();
+blue.src = Blue;
+let green = new Image();
+green.src = Green
+let purple = new Image();
+purple.src = Purple
+let red = new Image();
+red.src = Red
+let yellow = new Image();
+yellow.src = Yellow;
+let fillColors = [blue, green, purple, red, yellow];
 
 //с = необходимое количество цветов (4)
 fillColors = fillColors.slice(0, c);
 
-
-
 var rects = [];
+
+let pointsField = document.querySelector(".points-counter");
+let movesField = document.querySelector(".moves-counter");
+pointsField.innerHTML = itemsToDestroy;
+movesField.innerHTML = movesToWin;
 
 class fieldItem {
   constructor(x, y, width, height, fillColor, row, col) {
@@ -42,52 +59,58 @@ class fieldItem {
   }
 
   draw() {
-    context.strokeStyle = "rgba(255, 255, 255, 0.5)";
-    context.fillStyle = fillColors[this.fillColor];
-    context.fillRect(this.x, this.y, this.width, this.height);
-  };
+    context.drawImage(
+      fillColors[this.fillColor],
+      this.x,
+      this.y,
+      this.height,
+      this.width
+    );
+  }
 
   delete() {
-    // context.strokeStyle = "rgba(255, 255, 255, 0.5)";
-    // context.fillStyle = "rgba(255, 255, 255)";
-    // context.fillRect(this.x, this.y, this.width, this.height);
-    this.fillColor = null
+    this.fillColor = null;
     context.clearRect(this.x, this.y, this.width, this.height);
   }
 
   getRow() {
-    return this.row
+    return this.row;
   }
 
   moveDown() {
-    this.row = this.row+1
-    this.y = this.y + itemSize
-    this.update()
+    this.row = this.row + 1;
+    this.y = this.y + itemSize;
+    this.update();
   }
 
   moveUp() {
-    this.row = this.row-1
-    this.y = this.y - itemSize
+    this.row = this.row - 1;
+    this.y = this.y - itemSize;
     context.clearRect(this.x, this.y, this.width, this.height);
   }
 
   getCol() {
-    return this.col
+    return this.col;
   }
 
   getColor() {
-    return this.fillColor
+    return this.fillColor;
   }
 
   itemClick(eventX, eventY) {
-      return (eventX > this.x && eventX < this.x+itemSize && eventY > this.y && eventY < this.y+itemSize)
+    return (
+      eventX > this.x &&
+      eventX < this.x + itemSize &&
+      eventY > this.y &&
+      eventY < this.y + itemSize
+    );
   }
 
   setColor(color) {
     this.fillColor = color;
     this.update();
   }
-  
+
   update() {
     this.draw();
   }
@@ -96,8 +119,16 @@ class fieldItem {
 function initPlayField() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      let rect = new fieldItem(i * itemSize, j * itemSize, itemSize, itemSize, getRndInteger(0, fillColors.length - 1), j, i);
-      rects.push(rect);     
+      let rect = new fieldItem(
+        i * itemSize,
+        j * itemSize,
+        itemSize,
+        itemSize,
+        getRndInteger(0, fillColors.length - 1),
+        j,
+        i
+      );
+      rects.push(rect);
     }
   }
   if (checkArray(rects)) {
@@ -107,19 +138,19 @@ function initPlayField() {
   } else {
     rects = [];
     initPlayField();
-    console.log('restructure')
-  }  
+    console.log("restructure");
+  }
+  console.log(rects);
 }
 
-window.onload = initPlayField()
-
+yellow.onload = initPlayField;
 function checkArray(rects) {
   for (let r of rects) {
-    if (checkItem(r).length>=k) {
-      return true
+    if (checkItem(r).length >= k) {
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 //рандом для выбора цвета
@@ -129,63 +160,113 @@ function getRndInteger(min, max) {
 
 //проверка поля на наличие комбинаций относительно элемента
 function checkItem(r) {
-  let visited = []
-  let sameColorItems = []
-  let queue = []
-  
+  let visited = [];
+  let sameColorItems = [];
+  let queue = [];
+
   queue.push(r);
   visited.push(r);
   sameColorItems.push(r);
 
-  while(queue.length > 0) {
+  while (queue.length > 0) {
     //проверяем первый элемент в очереди
-    let v = queue.shift()
+    let v = queue.shift();
     //находим соседние для него не посещенные элементы
     for (let item of rects) {
       if (!visited.includes(item)) {
-        if ( Math.abs(item.getCol()-v.getCol()) == 1 && item.getRow() == v.getRow() 
-        || ( Math.abs(item.getRow()-v.getRow()) == 1 && item.getCol() == v.getCol() )) {
+        if (
+          (Math.abs(item.getCol() - v.getCol()) == 1 &&
+            item.getRow() == v.getRow()) ||
+          (Math.abs(item.getRow() - v.getRow()) == 1 &&
+            item.getCol() == v.getCol())
+        ) {
           //добавляем их в список посещенных
-          visited.push(item)
-          //находим среди них элементы такого же цвета добавляем их в соответствующий список и в очередь для обработки 
-          if (item.getColor()==v.getColor()) {
-            sameColorItems.push(item)
-            queue.push(item)                  
-          } 
-        } 
+          visited.push(item);
+          //находим среди них элементы такого же цвета добавляем их в соответствующий список и в очередь для обработки
+          if (item.getColor() == v.getColor()) {
+            sameColorItems.push(item);
+            queue.push(item);
+          }
+        }
       }
-    }         
+    }
   }
   return sameColorItems;
 }
 
-let indicesToDelete = []
-let itN = 1
+//проверка на "висящие" элементы
+function floatingItemsCheck() {
+  let floatingItems = [];
+  for (let floatingItem = 0; floatingItem < rects.length - 1; floatingItem++) {
+    if (
+      rects[floatingItem].getColor() != null &&
+      rects[floatingItem + 1].getColor() == null &&
+      rects[floatingItem + 1].getCol() == rects[floatingItem].getCol() &&
+      floatingItem + 1 <= rects.length - 1
+    ) {
+      floatingItems.push(rects[floatingItem]);
+    }
+  }
+  return floatingItems;
+}
 
 //перемещение элементов после удаления
 function itemFall() {
-  for (let t=0; t<rects.length-1; t++) {
-    if (rects[t].getColor() != null && rects[t+1].getColor() == null && rects[t+1].getCol() == rects[t].getCol() && (t+1)<=(rects.length-1)) {
-      let x = 1;
-      //помашоговое перемещение элемента на одну позицию вниз если она пустая
-      while ((t+x)<=(rects.length-1) && rects[t+x].getColor() == null && rects[t+x].getCol() == rects[t].getCol()) {
-        rects[t].moveDown()
-        rects[t+x].moveUp()
-        x+=1
+  let floatingItems = floatingItemsCheck();
+  while (floatingItems.length > 0) {
+    for (let t = 0; t < rects.length - 1; t++) {
+      if (
+        rects[t].getColor() != null &&
+        rects[t + 1].getColor() == null &&
+        rects[t + 1].getCol() == rects[t].getCol() &&
+        t + 1 <= rects.length - 1
+      ) {
+        let x = 1;
+        floatingItems.pop();
+        //помашоговое перемещение элемента на одну позицию вниз если она пустая
+        while (
+          t + x <= rects.length - 1 &&
+          rects[t + x].getColor() == null &&
+          rects[t + x].getCol() == rects[t].getCol()
+        ) {
+          rects[t].moveDown();
+          rects[t + x].moveUp();
+          x += 1;
+        }
+        //сортировка обработанных элементов в массиве по строке
+        let tempArray = rects.slice(t, t + x).sort((a, b) => {
+          return a.getRow() - b.getRow();
+        });
+        rects.splice(t, x, ...tempArray);
       }
-      //сортировка обработанных элементов в массиве по строке
-      let tempArray = rects.slice(t, t+x)
-      .sort((a, b) => {return a.getRow() - b.getRow();})           
-      rects.splice(t, x, ...tempArray)            
+    }
+    floatingItems = floatingItemsCheck();
+  }
+}
+
+//репопуляция поля
+function populateEmptyItems() {
+  for (let emptyItem of rects) {
+    if (emptyItem.getColor() == null) {
+      setTimeout(
+        emptyItem.setColor.bind(emptyItem),
+        600,
+        getRndInteger(0, fillColors.length - 1)
+      );
     }
   }
-  //"висящих" объектов после выполения не может быть больше чем строк, поэтому просто рекурсивно вызываем функцию пока все строки не проверены 
-  itN+=1
-  if (itN <rows) {
-    itemFall()
+}
+
+//обновление поля info
+function infoUpdate(sameColorItems) {
+  itemsToDestroy = itemsToDestroy - sameColorItems.length;
+  movesToWin--;
+  if (itemsToDestroy < 0) {
+    pointsField.innerHTML = 0;
   } else {
-    itN = 1
+    pointsField.innerHTML = itemsToDestroy;
   }
+  movesField.innerHTML = movesToWin;
 }
 
 //обработка клика на элемент
@@ -193,33 +274,23 @@ canvas.addEventListener("click", (event) => {
   const x = event.clientX;
   const y = event.clientY;
   for (let r of rects) {
-    let state = r.itemClick(x,y);
-    if (state) {           
+    let state = r.itemClick(x, y);
+    if (state) {
       let sameColorItems = checkItem(r);
       if (sameColorItems.length >= k) {
         for (let i of sameColorItems) {
           //удалить с canvas
           i.delete();
-          // context.delete(i)
-          //поиск индексов для удаления из массива
-          // let index = rects.findIndex(function(value, index) {
-          //   return (value == i)
-          // })
-          // indicesToDelete.push(index);
-          // delete rects[index];
-        // setTimeout(i.setColor.bind(i), 600, (getRndInteger(0, fillColors.length - 1)))             
         }
+        //обновление поля info
+        infoUpdate(sameColorItems);
       }
+
+      //перемещение элементов после удаления
+      itemFall();
+
+      //репопуляция поля
+      populateEmptyItems();
     }
   }
-  //удаление элементов из массива
-  for (let i of indicesToDelete) {
-    delete rects[i]
-  }
-
-  //передвижение элементов
-  itemFall();
-  
-
-  //добавление новых элементов
 });
